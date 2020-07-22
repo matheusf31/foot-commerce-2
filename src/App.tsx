2da5f9b186a1dff2d7608cd49bcb68ee8d3fc7fb
Matheus Filipe <mfcr031@gmail.com>
2020-07-21 22:41:06 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+import background from '../assets/images/background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    /* background: #191924 url(${background}) no-repeat center top; */
+    background: #17181d;
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+`;
