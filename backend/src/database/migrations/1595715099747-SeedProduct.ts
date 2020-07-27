c7f0d2baca0db2e62fab39652a18265e718eca8c
Matheus Filipe <mfcr031@gmail.com>
2020-07-27 20:11:27 -0300

    update

--- backend/src/database/migrations/1595715099747-SeedProduct.ts
+++ backend/src/database/migrations/1595715099747-SeedProduct.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
-import { productsSeed } from '../seeds/product.seed';
+// import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
+// import { productsSeed } from '../seeds/product.seed';
 
-export default class SeedProduct1595715099747 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await getRepository('products').save(productsSeed);
-  }
+// export default class SeedProduct1595715099747 implements MigrationInterface {
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await getRepository('products').save(productsSeed);
+//   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
+//   public async down(queryRunner: QueryRunner): Promise<void> {}
+// }
